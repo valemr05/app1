0b725dfee02adddd46216f50a5c3b563b155859c
valemr05 <valeria.munoz3@utp.edu.co>
2025-07-23 18:24:46 -0500

    replicador component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,16 @@ import { InterpolacionComponent } from "./interpolacion/interpolacion.component"
 import { PadreComponent } from "./padre/padre.component";
 import { HijoComponent } from "./hijo/hijo.component";
 import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NuevoComponenteComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent], //importar los nuevos componentes
+  imports: [NuevoComponenteComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent, ReplicadorComponent], //importar los nuevos componentes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Universidad Tecnologica';
+  title = 'Property Binding en Angular';
 }
---
+++ src/app/replicador/replicador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-replicador',
+  standalone: true,
+  imports: [],
+  templateUrl: './replicador.component.html',
+  styleUrl: './replicador.component.css'
+})
+export class ReplicadorComponent {
+
+}
